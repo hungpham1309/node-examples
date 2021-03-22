dfb578d8111c52d34b0fa3d1a698c99ff2f124ca
hungpc <hungpc@sendo.vn>
2021-03-22 20:26:46 +0700

    init-project

---
+++ index.js
@@ -0,0 +1,21 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = " + l + "and b = " + b);
+
+    if ( l <= 0 || b <= 0) {
+        console.log("Rectangle dimensions should be greater than 0");
+
+    }
+    else{
+        console.log("Area of the rect : " + rect.area(l,b));
+        console.log("Perimeter is: " +  rect.perimeter(l,b));
+    }
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
